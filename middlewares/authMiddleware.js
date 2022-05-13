8d3125ab828993a6a70599ac53e8bc98334e426f
rafaelmfranca <93008327+rafaelmfranca@users.noreply.github.com>
2022-05-13 18:59:43 -0300

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,5 @@
 function authMiddleware(req, res, next) {
   const { authorization } = req.headers;
-  console.log(req.headers);
-  console.log(authorization.length);
 
   if (!authorization || authorization.length !== 16) {
     return res.status(401).json({ message: 'Invalid token!' });
