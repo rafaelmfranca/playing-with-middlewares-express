851ed1b81d585e430dee9632f8b5ecf3ea9035f6
Rafael França <rafaelmonfardinifranca@gmail.com>
2022-05-13 17:30:41 -0300

    :sparkles: Exercises 1.4, 1.5, 1.7 and 1.10

--- middlewares/validateInfos.js
+++ middlewares/validateInfos.js
@@ -1,3 +1,15 @@
-function validateInfos(req, res, next) {}
+function validateInfos(req, res, next) {
+  const { infos } = req.body;
+
+  if (!infos) return res.status(400).json({ message: 'Infos is required!' });
+
+  if (!infos.saleDate)
+    return res.status(400).json({ message: 'Sale date is required!' });
+
+  if (!infos.warrantyPeriod)
+    return res.status(400).json({ message: 'Warranty period is required!' });
+
+  next();
+}
 
 module.exports = validateInfos;
